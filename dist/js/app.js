7c6d4b639581e4fccd26b735db414e0870d79894
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-20 13:04:13 +0000

    video1.6

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,4 @@
 import { Negociacao } from './models/negociacao.js';
 
 const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao);
-negociacao.quantidade = 10000;
-console.log(negociacao);
+console.log(negociacao.volume);
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -8,4 +8,20 @@ export class Negociacao {
         this.#quantidade = quantidade;
         this.#valor = valor;
     }
+
+    get data() {
+        return this.#data;
+    }
+
+    get quantidade() {
+        return this.#quantidade;
+    }
+
+    get valor() {
+        return this.#valor;
+    }
+
+    get volume() {
+        return this.#quantidade * this.#valor;
+    }
 }
