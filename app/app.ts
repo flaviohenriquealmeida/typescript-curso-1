81db2d6410d33d2d3e44c0001b338226a164f88f
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-20 14:04:43 +0000

    video2.4

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,4 @@
 import { Negociacao } from './models/negociacao.js';
 
-const negociacao = new Negociacao(new Date());
+const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao.volume);
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,3 @@
 import { Negociacao } from './models/negociacao.js';
-const negociacao = new Negociacao(new Date());
+const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao.volume);
