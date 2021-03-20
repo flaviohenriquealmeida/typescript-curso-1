dcaca07a7f9c842f891796372160ab051a51cd0a
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-20 14:56:44 +0000

    video2.6

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,27 @@
 export class Negociacao {
-    #data;
-    #quantidade;
-    #valor;
+    private _data;
+    private _quantidade;
+    private _valor;
 
     constructor(data, quantidade, valor) {
-        this.#data = data;
-        this.#quantidade = quantidade;
-        this.#valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
     get data() {
-        return this.#data;
+        return this._data;
     }
 
     get quantidade() {
-        return this.#quantidade;
+        return this._quantidade;
     }
 
     get valor() {
-        return this.#valor;
+        return this._valor;
     }
 
     get volume() {
-        return this.#quantidade * this.#valor;
+        return this._quantidade * this._valor;
     }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,37 +1,19 @@
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to set private field on non-instance");
-    }
-    privateMap.set(receiver, value);
-    return value;
-};
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to get private field on non-instance");
-    }
-    return privateMap.get(receiver);
-};
-var _data, _quantidade, _valor;
 export class Negociacao {
     constructor(data, quantidade, valor) {
-        _data.set(this, void 0);
-        _quantidade.set(this, void 0);
-        _valor.set(this, void 0);
-        __classPrivateFieldSet(this, _data, data);
-        __classPrivateFieldSet(this, _quantidade, quantidade);
-        __classPrivateFieldSet(this, _valor, valor);
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
     get data() {
-        return __classPrivateFieldGet(this, _data);
+        return this._data;
     }
     get quantidade() {
-        return __classPrivateFieldGet(this, _quantidade);
+        return this._quantidade;
     }
     get valor() {
-        return __classPrivateFieldGet(this, _valor);
+        return this._valor;
     }
     get volume() {
-        return __classPrivateFieldGet(this, _quantidade) * __classPrivateFieldGet(this, _valor);
+        return this._quantidade * this._valor;
     }
 }
-_data = new WeakMap(), _quantidade = new WeakMap(), _valor = new WeakMap();
