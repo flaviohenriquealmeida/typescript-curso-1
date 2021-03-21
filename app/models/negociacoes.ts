4be9309d623bd611c64d08db0661f62eeda69a66
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-21 12:58:13 +0000

    video5.2

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from './negociacao.js';
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
-    lista(): ReadonlyArray<Negociacao> {
+    lista(): readonly Negociacao[] {
         return this.negociacoes;
     }
 }
