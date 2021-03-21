0a36bf834f22e0ff8b9f8881ba084df75c88c265
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-21 13:06:29 +0000

    video5.3

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,23 +1,11 @@
 export class Negociacao {
     constructor(
-        private _data: Date, 
-        private _quantidade: number, 
-        private _valor: number
+        public readonly data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor: number
     ) {}
 
-    get data(): Date {
-        return this._data;
-    }
-
-    get quantidade(): number {
-        return this._quantidade;
-    }
-
-    get valor(): number {
-        return this._valor;
-    }
-
     get volume(): number {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,19 +1,10 @@
 export class Negociacao {
-    constructor(_data, _quantidade, _valor) {
-        this._data = _data;
-        this._quantidade = _quantidade;
-        this._valor = _valor;
-    }
-    get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
+    constructor(data, quantidade, valor) {
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
