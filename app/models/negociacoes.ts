42c1845b95d333ffcbb9a55ce06d560a023c14c1
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-21 12:02:16 +0000

    video4.3

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -2,4 +2,12 @@ import { Negociacao } from './negociacao.js';
 
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = [];
+
+    adiciona(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): Array<Negociacao> {
+        return this.negociacoes;
+    }
 }
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -2,4 +2,10 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = [];
     }
+    adiciona(negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+    lista() {
+        return this.negociacoes;
+    }
 }
